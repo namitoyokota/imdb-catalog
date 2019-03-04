45879576aa55c0323e64084e75dccc2b9ab1864b
Namito Yokota <nyokota@crimson.ua.edu>
2019-03-04 13:03:14 -0600

    crud template

---
+++ src/crud.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void createData(int index, char* type, char* title, char* adult, char* startYear, char* endYear, char* runtime, char* genres) {
+    return;
+}
+
+void retrieveData(int index) {
+    return;
+}
+
+void updateData(int index, char* type, char* title, char* adult, char* startYear, char* endYear, char* runtime, char* genres) {
+    return;
+}
+
+void deleteData(int index) {
+    return;
+}  
---
+++ src/crud.h
@@ -0,0 +1,11 @@
+#ifndef crud_h
+#define crud_h
+
+#include <stdio.h>
+
+extern void createData();
+extern void retrieveData();
+extern void updateData();
+extern void deleteData();
+
+#endif
