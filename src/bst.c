fb85fcb80361e6c9cbd54774509f977fdfc1f2a0
Namito Yokota <nyokota@crimson.ua.edu>
2019-03-04 13:04:15 -0600

    updated struct and different sorting methods

--- src/bst.c
+++ src/bst.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include "bst.h"
-#include "queue.h"
+//#include "queue.h"
 
 struct node
 {
 	bool enable;
 	int index;
-	char *title;
 	char *type;
+	char *title;
+	bool adult;
+	char *startYear;
+	char *endYear;
+	char *runtime;
 	char *genres;
 	NODE* left;
 	NODE* right;
 };
 
-NODE* NewNODE(int index, char* title, char* type, char* genres)
+NODE* NewNODE (int index, char* type, char* title, char* adult, char* startYear, char* endYear, char* runtime, char* genres)
 {
 	NODE* temp = (NODE*)malloc(sizeof(NODE));
 	temp->enable = true;
 	temp->index = index;
-	temp->title = title;
 	temp->type = type;
+	temp->title = title;
+	temp->adult = atoi(adult);
+	temp->startYear = startYear;
+	temp->endYear = endYear;
+	temp->runtime = runtime;
 	temp->genres = genres;
 	temp->left = NULL;
 	temp->right = NULL;
@@ -29,106 +38,100 @@ NODE* NewNODE(int index, char* title, char* type, char* genres)
 	return temp;
 }
 
-NODE* insert(NODE* bst, int index, char* title, char* type, char* genres)
+NODE* insert (int sort, NODE* bst, int index, char* type, char* title, char* adult, char* startYear, char* endYear, char* runtime, char* genres)
 {
-	if(bst==NULL)
-		bst =  NewNODE(index, title, type, genres);
-
-	else if(index <= bst->index)
-		bst->left = insert(bst->left,index, title, type, genres);
-	
-	else bst->right = insert(bst->right,index, title, type, genres);
-
-	return bst;
-
-}
-
-NODE* Delete(NODE* bst,int index)
-{
-	if(bst==NULL)
+	if (bst==NULL) {
+		bst =  NewNODE(index, type, title, adult, startYear, endYear, runtime, genres);
 		return bst;
-
-	else if(index < bst->index)
-		bst->left = Delete(bst->left,index);
-
-	else if(index > bst->index)
-		bst->right = Delete(bst->right,index);
-
-	else
-	{
-		if(bst->left==NULL && bst->right==NULL)
-		{
-			free(bst);
-			bst = NULL;
-		}
-
-		else if(bst->left!=NULL && bst->right==NULL)
-		{
-			NODE* temp = bst->left;
-			free(temp);
-			bst = bst->left;
-		}
-		else if(bst->right!=NULL&& bst->left==NULL)
-		{
-			NODE* temp = bst->right;
-			free(temp);
-			bst = bst->right;
-		}
-
-		else
-		{
-			NODE* temp = FindMin(bst->right);
-			bst->index = temp->index;
-			bst->right = Delete(bst->right,temp->index); 
-		}
-
 	}
+
+	switch(sort) {
+		case 1 : // insert by index
+			if (index <= bst->index)
+				bst->left = insert(sort, bst->left, index, type, title, adult, startYear, endYear, runtime, genres);
+			else
+				bst->right = insert(sort, bst->right, index, type, title, adult, startYear, endYear, runtime, genres);
+			break;
+
+		case 3 : // insert by title
+			for (int i=0; i<(int)strlen(title); i++) {
+				if (title[i] == bst->title[i]) continue;
+				else if (title[i] < bst->title[i]) {
+					bst->left = insert(sort, bst->left, index, type, title, adult, startYear, endYear, runtime, genres);
+					break;
+				}
+				else if (title[i] > bst->title[i]) {
+					bst->right = insert(sort, bst->right, index, type, title, adult, startYear, endYear, runtime, genres);
+					break;
+				}
+			}
+			break;
+
+		case 6 : // insert by start year
+			for (int i=0; i<(int)strlen(startYear); i++) {
+				if (startYear[i] == bst->startYear[i]) continue;
+				else if (startYear[i] < bst->startYear[i]) {
+					bst->left = insert(sort, bst->left, index, type, title, adult, startYear, endYear, runtime, genres);
+					break;
+				}
+				else if (startYear[i] > bst->startYear[i]) {
+					bst->right = insert(sort, bst->right, index, type, title, adult, startYear, endYear, runtime, genres);
+					break;
+				}
+			}
+			break;
+		
+		case 7 : // insert by end year
+			for (int i=0; i<(int)strlen(endYear); i++) {
+				if (endYear[i] == bst->endYear[i]) continue;
+				else if (endYear[i] < bst->endYear[i]) {
+					bst->left = insert(sort, bst->left, index, type, title, adult, startYear, endYear, runtime, genres);
+					break;
+				}
+				else if (endYear[i] > bst->endYear[i]) {
+					bst->right = insert(sort, bst->right, index, type, title, adult, startYear, endYear, runtime, genres);
+					break;
+				}
+			}
+			break;
+		
+		case 8 : // insert by end year
+			for (int i=0; i<(int)strlen(runtime); i++) {
+				if (runtime[i] == bst->runtime[i]) continue;
+				else if (runtime[i] < bst->runtime[i]) {
+					bst->left = insert(sort, bst->left, index, type, title, adult, startYear, endYear, runtime, genres);
+					break;
+				}
+				else if (runtime[i] > bst->runtime[i]) {
+					bst->right = insert(sort, bst->right, index, type, title, adult, startYear, endYear, runtime, genres);
+					break;
+				}
+			}
+			break;
+
+		default:
+			break;
+	}
+
 	return bst;
 }
 
-NODE* FindMin(NODE* bst)
+void InOrder (NODE* bst)
 {
-	while(bst->left!=NULL)
-		bst = bst->left;
-
-	return bst;
-}
-
-void PreOrder(NODE* bst)
-{
-	if(bst==NULL)
-		return;
-
-	printf("\n%d, %d, %s, %s, %s", bst->enable, bst->index, bst->title, bst->type, bst->genres);
-	PreOrder(bst->left);
-	PreOrder(bst->right);
-}
-
-void InOrder(NODE* bst)
-{
-	if(bst==NULL)
+	if (bst==NULL)
 		return;
 
 	InOrder(bst->left);
-	printf("\n%d, %d, %s, %s, %s", bst->enable, bst->index, bst->title, bst->type, bst->genres);
+	printf("\n%d \t %d \t %s \t %s \t %d \t %s \t %s \t %s \t %s", bst->enable, bst->index, bst->type, bst->title, bst->adult, bst->startYear, bst->endYear, bst->runtime, bst->genres);
 	InOrder(bst->right);
 }
 
-void PostOrder(NODE* bst)
+void NotOrder (NODE* bst)
 {
-	if(bst==NULL)
+	if (bst==NULL)
 		return;
 
-	PostOrder(bst->left);
-	PostOrder(bst->right);
-	printf("\n%d, %d, %s, %s, %s", bst->enable, bst->index, bst->title, bst->type, bst->genres);
-}
-
-
-void traverse(NODE* bst)
-{
-	printf("\nPreOrder traversal : "); PreOrder(bst);
-	printf("\n\nInorder traversal  :   "); InOrder(bst);
-	printf("\n\nPostOrder traversal:   "); PostOrder(bst);
-	printf("\n");
+	NotOrder(bst->right);
+	printf("\n%d \t %d \t %s \t %s \t %d \t %s \t %s \t %s \t %s", bst->enable, bst->index, bst->type, bst->title, bst->adult, bst->startYear, bst->endYear, bst->runtime, bst->genres);
+	NotOrder(bst->left);
 }
--- src/bst.h
+++ src/bst.h
@@ -5,14 +5,15 @@
 
 typedef struct node NODE;
 
-extern NODE* NewNODE(int, char*, char*, char*);
+extern NODE* NewNODE(int, char*, char*, char*, char*, char*, char*, char*);
 extern void PostOrder(NODE*);
 extern NODE* FindMin(NODE*);
 extern void LevelOrder(NODE*);
 extern NODE* Delete(NODE*,int);
 extern void InOrder(NODE*);
+extern void NotOrder(NODE*);
 extern void PreOrder(NODE*);
-extern NODE* insert(NODE*,int, char*, char*, char*);
+extern NODE* insert(int, NODE*, int, char*, char*, char*, char*, char*, char*, char*);
 extern void traverse(NODE*);
 
 #endif
